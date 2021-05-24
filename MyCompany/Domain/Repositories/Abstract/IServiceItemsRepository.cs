baef727cb08b289cf4add50941fae0b1e3e89db5
unknown <@408-08.kpc408.ru>
2021-05-24 10:31:57 +0300

    aadfgh

---
+++ MyCompany/Domain/Repositories/Abstract/IServiceItemsRepository.cs
@@ -0,0 +1,14 @@
+﻿using EBook.Domain.Entities;
+using System;
+using System.Linq;
+
+namespace EBook.Domain.Repositories.Abstract
+{
+    public interface IServiceItemsRepository
+    {
+        IQueryable<ServiceItem> GetServiceItems();
+        ServiceItem GetServiceItemById(Guid id);
+        void SaveServiceItem(ServiceItem entity);
+        void DeleteServiceItem(Guid id);
+    }
+}
---
+++ MyCompany/Domain/Repositories/Abstract/ITextFieldsRepository.cs
@@ -0,0 +1,15 @@
+﻿using EBook.Domain.Entities;
+using System;
+using System.Linq;
+
+namespace EBook.Domain.Repositories.Abstract
+{
+    public interface ITextFieldsRepository
+    {
+        IQueryable<TextField> GetTextFields();
+        TextField GetTextFieldById(Guid id);
+        TextField GetTextFieldByCodeWord(string codeWord);
+        void SaveTextField(TextField entity);
+        void DeleteTextField(Guid id);
+    }
+}
